1f589a2079e49caeaf706d929e46aece0902b1c7
larabelarmino <lara.cbdm@gmail.com>
2017-07-10 11:40:46 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,46 @@ import {SamuService} from './services/samu.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UFService, SamuService]
 })
+
 export class AppComponent implements OnInit {
-    title = 'app';
     ufs : UF[];
     dados_da_samu : Dados[];
+    minha_uf: UF;
+    municipios_atendidos: Dados[] = [];
+    meu_id = 31;
+    media: number = 0;
 
     constructor(private ufService: UFService, private samuService: SamuService)
-    { }
+    {
+
+    }
 
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.minha_uf = this.defineUF();
+        this.media = this.calcularMunicipios();
+    }
+
+    defineUF(): UF {
+      for (let uf of this.ufs) {
+          if (uf.id == this.meu_id) return uf;
+      }
+    }
+
+    calcularMunicipios(): number {
+      var qnt = 0;
+      var total = 0;
+      for (let mun of this.dados_da_samu){
+        if (mun.uf_id == this.meu_id){
+          qnt++;
+          total+=mun.valor;
+          this.municipios_atendidos.push(mun);
+        }
+      }
+        return total/qnt;
     }
 }
